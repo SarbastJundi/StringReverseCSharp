959498bf6cc49a7f8e3caa6632afd23c64155cc7
Sarbast Jundi <sarbast.k.jundi@gmail.com>
2018-05-16 10:52:59 +0200

    up

---
+++ ReverseAString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReverseAString
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			String str = "hello";
+            String reverse = " ";
+			for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reverse = reverse + str[i];
+            }
+            
+			Console.WriteLine(reverse);
+        }
+    }
+}
